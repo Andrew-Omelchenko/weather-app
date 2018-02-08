87e30638f6ad565cbd41f0f51a82f2dd6ebc8329
Andrew Omelchenko <andrewom43@gmail.com>
2018-02-08 19:27:58 +0200

    refactor classes, add inheritance.

--- js/services/favorites_service.js
+++ js/services/favorites_service.js
@@ -1,17 +1,6 @@
-class FavoritesService {
+class FavoritesService extends ListService {
   constructor(storageSvc, name) {
-    this._storageService = storageSvc;
-    this._name = name;
-    this._data = this._storageService.read(this._name);
-    if (this._data == null) {
-      this._data = [];
-    }
-  }
-
-  get data() {
-    console.log("FavoritesService. Getting favorites");
-    console.log(this._data);
-    return this._data;
+    super(storageSvc, name);
   }
 
   add(item) {
@@ -30,28 +19,8 @@ class FavoritesService {
     this._data.push(item);
     this._data.sort();
     this._storageService.write(this._data, this._name);
+    console.log("Favorites service. Adding favorite.");
     console.log(this._data);
     return true;
-  }
-
-  remove(item) {
-    if (!this._data || this._data.length == 0) {
-      return;
-    }
-    let tmp = [];
-    for (let elem of this._data) {
-      if (elem != item && elem) {
-        tmp.push(elem);
-      }
-    }
-    // since old array was sorted, the new array also is
-    this._data = tmp;
-    this._storageService.write(this._data, this._name);
-  }
-
-  clear() {
-    this._storageService.remove(this._name);
-    this._data = [];
-    console.log(this._data);
   }
 }
--- js/services/history_service.js
+++ js/services/history_service.js
@@ -1,17 +1,6 @@
-class HistoryService {
+class HistoryService extends ListService {
   constructor(storageSvc, name) {
-    this._storageService = storageSvc;
-    this._name = name;
-    this._data = this._storageService.read(this._name);
-    if (this._data == null) {
-      this._data = [];
-    }
-  }
-
-  get data() {
-    console.log("HistoryService. Getting history");
-    console.log(this._data);
-    return this._data;
+    super(storageSvc, name);
   }
 
   add(item) {
@@ -37,27 +26,8 @@ class HistoryService {
     // add item
     this._data.push(item);
     this._storageService.write(this._data, this._name);
+    console.log("History service. Adding history item.");
     console.log(this._data);
     return true;
-  }
-
-  remove(item) {
-    if (!this._data || this._data.length == 0) {
-      return;
-    }
-    let tmp = [];
-    for (let elem of this._data) {
-      if (elem != item && elem) {
-        tmp.push(elem);
-      }
-    }
-    this._data = tmp;
-    this._storageService.write(this._data, this._name);
-  }
-
-  clear() {
-    this._storageService.remove(this._name);
-    this._data = [];
-    console.log(this._data);
   }
 }
